6ff238136e67de348925271ab93822566bb4cb78
jcfdantas <91327490+jcfdantas@users.noreply.github.com>
2025-04-22 09:20:52 -0300

    Update matriz copy.py

--- matriz copy.py
+++ matriz copy.py
@@ -6,8 +6,13 @@ B = [
 ]
 
 soma = [[A[i][j] + B[i][j] for j in range(len(A[0]))] for i in range (len(A))]
+
 #for j in range(len=(A[0])) percorre a A[0] e j percorre a quantidade de numeros em A[0]
+
 #for i in range (len(A)) quantidade de "vetores" na matriz A 
+
+#tudo em matriz[], range(), len()
+
 print("Resultado da soma de matrizes é: ")
 print(soma)
 
