a893cdc29ccd40c6fc1f2cee3b27876f9e17161c
jcfdantas <91327490+jcfdantas@users.noreply.github.com>
2025-04-22 09:38:33 -0300

    Update matriz copy.py

--- matriz copy.py
+++ matriz copy.py
@@ -6,7 +6,7 @@ B = [
 ]
 
 soma = [[A[i][j] + B[i][j] for j in range(len(A[0]))] for i in range (len(A))]
-
+# jeito para imprimir mais bonitinho print(f"A[{i}][{j}] + B[{i}][{j}] = {A[i][j]} + {B[i][j]}")
 #for j in range(len=(A[0])) percorre a A[0] e j percorre a quantidade de numeros em A[0]
 
 #for i in range (len(A)) quantidade de "vetores" na matriz A 
