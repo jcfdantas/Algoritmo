59f8000fa7863e9ab203dac6af958f9f138dbec8
jcfdantas <91327490+jcfdantas@users.noreply.github.com>
2025-05-07 09:58:55 -0300

    Update recebe_tamanho.py

--- recebe_tamanho.py
+++ recebe_tamanho.py
@@ -1,24 +1,31 @@
-m =[]
-tamanho = int(input("Digite o tamanho da matriz: "))
-for i in range(tamanho):
-    linha=[]
-    for j in range(tamanho):
-       vlr=int(input(f"Digite o valor para a posição[{i+1}][{j+1}]: "))
-       linha.append(vlr)
+m = []
+t = int(input("Digiteo tamanho da matriz: "))
+for i in range(t):
+    linha = []
+    for j in range(t):
+        v = int(input(f"Digite o valor da matriz para a posição [{i+1}][{j+1}]: "))
+        linha.append(v)
     m.append(linha)
-print(f"Essa é sua matriz inicial:{m}")
-diagonal_principal=[]
-soma_principal=0
-for i in range(len(m)):
-    diagonal_principal.append(m[i][i])
-    soma_principal += m[i][i]
-print(f"Diagonal principal: {diagonal_principal}")
-print(f"soma principal: {soma_principal}")
-
-d_s= []
-s_s=0
-for i in range(len(m)):
-    d_s.append(m[i][(len(m[1])) - 1 - i])
-    s_s += m[i][(len(m[1])) - 1 - i]
-print(f"Diagonal secundária: {d_s}")
-print(f"soma principalsecunsária = {s_s}")
+print("="*50)
+print(f"Essa é sua matriz inicial:")
+#para imprimir configurado é necessario usar for l in m: for v in l: print...
+for linha in m:
+        print("|", end=" ")
+        for v in linha:
+            print(f"{v:5}", end=" ")
+        print("|")
+s_p = 0
+d_p = []
+s_s = 0
+d_s = []
+for i in range(t):
+    d_p.append(m[i][i])
+    s_p += m[i][i]
+print(f"A diagonal principal é:\n{d_p}")
+print(f"A soma da diagonal principal é:\n{s_p}")
+for i in range(t):
+    d_s.append(m[i][(len(m[1])) -1 -i])
+    s_s += m[i][(len(m[1])) -1 -i]
+print(f"A diagonal secundária é:\n{d_s}")
+print(f"A soma da diagonal secundária é:\n{s_s}")
+print("="*50)
