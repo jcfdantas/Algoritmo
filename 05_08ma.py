8e00dd81e9f17a28d1529fbca04d9eb55aef3c04
jorge dantas <91327490+jcfdantas@users.noreply.github.com>
2025-08-05 10:37:57 -0300

    Add files via upload

---
+++ 05_08ma.py
@@ -0,0 +1,11 @@
+n = str(input("Digite um Número no qual será somado os dígitos: "))
+digito = str(input("Digite qual número vai ser contado: "))
+def somar (n, digito):
+    sum(int(digito) for figito in str(abs(n)))
+print (somar(n, digito))
+
+        
+        
+
+
+
